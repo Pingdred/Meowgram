01ac9ddeb6e577ea6ff372fe5a11b2447f28c6d0
Pingdred <ema.morrone01.em@gmail.com>
2023-11-18 21:15:30 +0100

    Update ccat_telegram_bot.py

--- ccat_telegram_bot.py
+++ ccat_telegram_bot.py
@@ -1,35 +1,84 @@
 import json
 import asyncio
-import requests
-import tempfile
 import logging
+import time
 
 import cheshire_cat_api as ccat
-from cheshire_cat_api.utils import Settings
+from cheshire_cat_api.utils import Settings, WebSocketSettings
 
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, MessageHandler
+from telegram.constants import ChatAction
 
-class CCatTelegramBot():
+T = 10
 
-    def __init__(self, telegram_token: str, ccat_url: str = "localhost", ccat_port: int = 1865) -> None:
+class CCatConnection:
 
-        self._loop = asyncio.get_event_loop()
+    def __init__(self, user_id, out_queue: asyncio.Queue, ccat_url: str = "localhost", ccat_port: int = 1865) -> None:
+        self.user_id = user_id
+
+        # Get event loop
+        self._loop = asyncio.get_running_loop()
 
         # Queue of the messages to send on telegram
-        self._out_queue = asyncio.Queue()
-
-        ccat_settings = Settings()
-
-        ccat_settings.base_url = ccat_url
-        ccat_settings.port = ccat_port
+        self._out_queue = out_queue
+        
+        ws_settings = WebSocketSettings(user_id=user_id)
+        ccat_settings = Settings(
+            base_url=ccat_url,
+            port=ccat_port,
+            ws=ws_settings
+        )
 
         # Instantiate the Cheshire Cat client
         self.ccat = ccat.CatClient(
             settings=ccat_settings,
             on_message=self._ccat_message_callback,
-            on_close=self._ccat_on_close
+            on_open=self._on_open,
+            on_close=self._on_close
         )
+
+        self.last_interaction = time.time()
+
+
+    def _ccat_message_callback(self, message: str):
+        # Websocket on_mesage callback
+
+        message = json.loads(message)
+
+        # Put the new message from the cat in the out queue
+        # the websocket runs in its own thread
+        # call_soon_threadsafe: https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.call_soon_threadsafe
+        #                       https://stackoverflow.com/questions/53669218/asyncio-queue-get-delay
+        self._loop.call_soon_threadsafe(self._out_queue.put_nowait, (message, self.user_id))
+    
+    
+    def _on_open(self):
+        logging.info(f"WS connection with user `{self.user_id}` to CheshireCat opened")
+
+
+    def _on_close(self, close_status_code: int, msg: str):
+        logging.info(f"WS connection `{self.user_id}` to CheshireCat closed")
+    
+
+    def send(self, message: str, **kwargs):
+        self.last_interaction = time.time()
+        self.ccat.send(message=message, **kwargs)
+
+
+class Meogram():
+
+    def __init__(self, telegram_token: str, ccat_url: str = "localhost", ccat_port: int = 1865) -> None:
+
+        self.ccat_url = ccat_url
+        self.ccat_port = ccat_port
+
+        self._loop = asyncio.get_running_loop()
+
+        # Queue of the messages to send on telegram
+        self._out_queue = asyncio.Queue()
+
+        self._connections = {}
 
         # Create telegram application
         self.telegram = ApplicationBuilder().token(telegram_token).build()
@@ -42,14 +91,15 @@ class CCatTelegramBot():
         self.telegram.add_handler(self.text_message_handler)
         self.telegram.add_handler(self.voice_message_handler)
 
-        self.sensory_url = "http://localhost:8500"
-
 
     async def run(self):
         try:
             await self.telegram.initialize()
             await self.telegram.updater.start_polling(read_timeout=10)  
             await self.telegram.start()
+
+            # Start closing idle ws connection each T seconds
+            self._loop.call_later(T, self._close_unactive_connections)
 
             responce_loop = self._loop.create_task(self._send_messages())
             await responce_loop
@@ -60,91 +110,108 @@ class CCatTelegramBot():
             await self.telegram.stop()
         finally:
             await self.telegram.shutdown()
-            self.ccat.close()
+            for connection in self._connections.values():
+                connection.ccat.close()
 
 
     async def _send_messages(self):
+
+        # Used to store for each connection when the last typing action is sended
+        last_typing_action = {}
+
         while True:
-            msg = await self._out_queue.get()
-            msg = json.loads(msg)
+            message, user_id = await self._out_queue.get()
 
-            await self.telegram.bot.send_message(
-                chat_id=msg["user_id"],
-                text=msg["content"]
-            )
+            try:
+                if message["type"] == "chat":
+                    # send the message in chat
+                    await self.telegram.bot.send_message(
+                        chat_id=user_id,
+                        text=message["content"],
+                    )
+                elif message['type'] == "chat_token":
+                    # Send the chat action TYPING every 5 seconds 
+                    # during the tokens streming
+                    t = time.time()
 
+                    if user_id not in last_typing_action:
+                        last_typing_action[user_id] = t - 5
+                        
+                    # Check elapsed time
+                    if t - last_typing_action[user_id] > 5:
 
-    def _ccat_message_callback(self, message: str):
-        # Websocket on_mesage callback
+                        logging.log(level=logging.INFO, msg=f"Sending chat action Typing to user {user_id}")
 
-        # Put the new message from the cat in the out queue
-        # the websocket runs in its own thread
-        # call_soon_threadsafe: https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.call_soon_threadsafe
-        #                       https://stackoverflow.com/questions/53669218/asyncio-queue-get-delay
-        self._loop.call_soon_threadsafe(self._out_queue.put_nowait, message)
-    
-    
-    def _ccat_on_close(self, close_status_code: int, msg: str):
-        logging.info("WS connection to CheshireCat closed")
-       
+                        # Update the time of the last typing action
+                        last_typing_action[user_id] = t
+
+                        # Send the action
+                        await self.telegram.bot.send_chat_action(
+                            chat_id=user_id,
+                            action=ChatAction.TYPING
+                        )
+            except Exception as e:
+                logging.error(f"An error occurred sending a telegram message: {e}")
+
 
     async def _text_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        # Send mesage to the cat
-        self.ccat.send(message=update.message.text, user_id=update.effective_chat.id)
+        chat_id = update.effective_chat.id
 
+        # Open a ws connection for the user if non exists
+        if chat_id not in self._connections:
+            self._connections[chat_id] = CCatConnection(
+                    user_id=chat_id,
+                    out_queue=self._out_queue,
+                    ccat_url=self.ccat_url,
+                    ccat_port=self.ccat_port
+                )
+
+        # Send mesage to the cat
+        self._connections[chat_id].send(message=update.message.text)
+        
 
     async def _voice_note_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-        userd_id = update.effective_chat.id
+        voice_message_id = update.message.voice.file_id
+        chat_id = update.effective_chat.id
 
-        # Extract audio bytes as bytearray
-        voice_file = await update.message.voice.get_file()
-        audio_bytes = await voice_file.download_as_bytearray(connect_timeout=10)
+        if chat_id not in self._connections:
 
-        try:
-            # Get text from audio
-            text = self._stt(file_name=update.message.voice.file_id, audio_bytes=audio_bytes)
+            self._connections[chat_id] = CCatConnection(
+                    user_id=chat_id,
+                    out_queue=self._out_queue,
+                    ccat_url=self.ccat_url,
+                    ccat_port=self.ccat_port
+                )
 
-            # Send mesage to the cat
-            self.ccat.send(message=text, user_id=userd_id)
-        except:
-            await self._out_queue.put({
-                "content": "I can't listen at the moment, could you send a text?",
-                "user_id": userd_id
-            })
+        # Send mesage to the cat
+        self._connections[chat_id].ccat.send(
+            message="*[Voice Note]* But you can't hear.",
+            meogram_voice=voice_message_id
+        )
 
 
-    def _stt(self, file_name: str, audio_bytes: bytearray): 
+    def _close_unactive_connections(self):
+            logging.info(msg="Closing inactive connections")
 
-        header = {
-            'accept': 'application/json',
-        }
-        files = {
-            'audio': (file_name, audio_bytes, "video/ogg")
-        }
-        
-        response = requests.post(f"{self.sensory_url}/stt", files=files, headers=header)
-        response = response.json()
-        
-        return response["text"]
-    
+            t = time.time()
 
-    def _tts(self, text: str):
-         
-        header = {
-            'accept': 'application/json',
-        }
-        params = {
-            'text': text
-        }
-        
-        response = requests.post(f"{self.sensory_url}/tts", params=params, headers=header)
+            marked_for_deletion = []
+            for key, conn in self._connections.items():
 
-        tmp_file_audio = tempfile.NamedTemporaryFile(suffix=".wav")
-        tmp_file_audio.write(response.content)
-        # song = AudioSegment.from_ogg(tmp.file.name)
-        # song.export("prova.wav", format="wav")
-        # TODO: try to handle .ogg directly
+                if conn.ccat.is_closed:
+                    marked_for_deletion.append(key)
+                    break
 
-        return tmp_file_audio
-        
+                if t - conn.last_interaction > 20:
+                    # Close the connection as mark it for delete
+                    conn.ccat.close()
+                    marked_for_deletion.append(key)
+
+            # Delete closed connections
+            for user_id in marked_for_deletion:
+                del self._connections[user_id]  
+
+            # Scheduling the next running
+            self._loop.call_later(T, self._close_unactive_connections)
+
