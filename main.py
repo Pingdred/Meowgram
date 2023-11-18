643efbd805663d59d8800602d742dd5953823f01
Pingdred <ema.morrone01.em@gmail.com>
2023-11-18 21:04:45 +0100

    Added CHESHIRE_CAT_URL and CHESHIRE_CAT_PORT in .env

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import logging
 import asyncio
 from dotenv import load_dotenv
 
-from ccat_telegram_bot import CCatTelegramBot
+from ccat_telegram_bot import Meogram
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+CCAT_URL = os.getenv("CHESHIRE_CAT_URL", "localhost")
+CCAT_PORT = os.getenv("CHESHIRE_CAT_PORT", "1865")
+
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
 async def main():
-    bot = CCatTelegramBot(TOKEN)
+    bot = Meogram(
+            telegram_token=TOKEN,
+            ccat_url=CCAT_URL,
+            ccat_port=CCAT_PORT                  
+        )
     await bot.run()
     
 if __name__ == "__main__":
