2b86221800b3301febdb9be51292bb7b30325258
Pingdred <ema.morrone01.em@gmail.com>
2025-02-08 22:56:34 +0100

    Moved telegram handlers in `MeowgramBot`

--- src/meowgram/bot.py
+++ src/meowgram/bot.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 import logging
 import asyncio
@@ -6,17 +7,16 @@ import tempfile
 import requests
 from typing import Dict
 
-from telethon import TelegramClient,  events, Button
+from telethon import TelegramClient, Button, Message
+from telethon.events import NewMessage, CallbackQuery, StopPropagation
 from telethon.tl.types import BotCommand, BotCommandScopeDefault
 from telethon.tl.functions.bots import SetBotCommandsRequest
 from cheshire_cat.client import CheshireCatClient
 
-from meowgram.handlers import command_handler, message_handler, form_action_handler
-from utils import audio_to_voice, CatFormState, clean_code_blocks
+from meowgram.madia_handlers import handle_unsupported_media, handle_chat_media, handle_file
+from utils import audio_to_voice, CatFormState, clean_code_blocks, clear_chat_history, build_base_cat_message
 
 class MeowgramBot:
-
-    # SECTION: Bot initialization
 
     def __init__(
         self, api_id: str, api_hash: str, bot_token: str, cat_url: str, cat_port: int
@@ -32,25 +32,156 @@ class MeowgramBot:
         
         self.logger = logging.getLogger(__name__)
 
+    # SECTION: Telegram event handlers
+
+    async def command_handler(self, event: CallbackQuery.Event):
+        """Handle commands"""
+        command = event.message.text
+        try:
+            match command:
+                case "/start":
+                    await event.reply("Welcome to Meowgram! How can I help you today?")
+
+                case "/clear":
+                    await clear_chat_history(self, event)
+        except Exception as e:
+            self.logger.error(f"An error occurred handling the command {command}: {e}")
+        finally:
+            raise StopPropagation
+
+    async def message_handler(self, event: NewMessage.Event):
+        """
+        Main handler for incoming Telegram messages.
+
+        Args:
+            meowgram: The main bot instance with necessary methods.
+            event: The incoming Telegram event containing the message.
+
+        Note:
+            This is the hierarchy of the message media types:
+
+            Message
+            ├── text                                    Text content of the message (can contain only text or a caption)
+            └── media                                   Media content attached to the message (if present)
+                ├── photo                               Image sent as a photo
+                ├── document                            Generic file content with extended attributes (can include various file types)
+                │   ├── video                           Video content with thumbnail and duration
+                │   │   └── video_note                  Round video message (usually 640x640)
+                │   ├── audio                           Audio file with metadata (title, performer)
+                │   │   └── voice                       Voice message recording
+                │   ├── sticker                         Image or animation sticker
+                │   │   ├── image/webp                  Image sticker (WebP format)
+                │   │   ├── video/webm                  Video sticker (WebM format)
+                │   │   └── application/x-tgsticker     Animated sticker (TGSticker format)
+                │   ├── gif                             Animated image (GIF)
+                ├── contact                             Shared contact information
+                ├── geo                                 Geographic coordinates (latitude and longitude)
+                │   └── venue                           Location with title and address
+                ├── poll                                Interactive poll
+                ├── web_preview                         Webpage preview (link preview)
+                ├── game                                Telegram game (interactive game)
+                ├── invoice                             Payment invoice (Telegram Payments)
+                └── dice                                Animated emoji/dice (used for sending dynamic emojis or dice)
+        """
+
+        user_id = event.sender_id
+        cat_client: CheshireCatClient = await self.ensure_cat_connection(user_id)
+        if not cat_client:
+            return
+
+        message: Message = event.message
+
+        # If no media is present, send the text message directly.
+        if not message.media:
+            base_msg = build_base_cat_message(event)
+            base_msg["text"] = message.text
+            await cat_client.send_message(base_msg)
+            return
+
+        # Define the handlers for different media types in order of priority.
+        handlers = [
+            handle_unsupported_media,
+            handle_chat_media,
+        ]
+
+        # Iterate through the handlers and return the first non-None result.
+        for handler in handlers:
+            result = await handler(event)
+
+            # Skip to the next handler if the result is None.
+            if result is None:
+                continue
+            
+            # Add the caption associated with the media if present.
+            if message.text:
+                result["text"] = message.text
+            await cat_client.send_message(result)
+
+            # Stop processing the message after sending the media
+            return
+
+        # Send every other media down the Rabbit Hole
+        if message.document:
+            # if message.text:
+            #     await message.reply("I'm reading the document, please wait a moment.")
+
+            await handle_file(event, cat_client)
+
+    async def form_action_handler(self, event: CallbackQuery.Event):
+        """Handle form actions"""
+        user_id = event.sender_id
+
+        cat_client = await self.ensure_cat_connection(user_id)
+        if not cat_client:
+            return
+
+        query = event.data.decode("utf-8")
+
+        pattern = r"^form_(?P<form_name>[a-zA-Z0-9_]+)_(?P<action>confirm|cancel)$"
+        match = re.match(pattern, query)
+
+        if not match:
+            return
+
+
+        form_name = match.group('form_name')
+        action = match.group("action")
+
+        message = build_base_cat_message(event)
+        message["text"] = action
+
+        form_action = {
+            "form_name": form_name,
+            "action": action #{}, # No information are needed for form actions
+        }
+        message["form_action"] = form_action
+
+        await cat_client.send_message(message)
+
+        await event.edit(buttons=None)
+
+
+    # SECTION: Bot initialization
+
     def set_telegram_handlers(self):
         """Setup event handlers"""
 
         # Handler for commands
         self.client.add_event_handler(
-            lambda event: command_handler(self, event), 
-            events.NewMessage(pattern=r"/.*")
+            self.command_handler, 
+            NewMessage(pattern=r"/.*")
         )
 
         # Handler for messages
         self.client.add_event_handler(
-            lambda event: message_handler(self, event),
-            events.NewMessage
+            self.message_handler,
+            NewMessage
         )
 
         # Handler for form actions
         self.client.add_event_handler(
-            lambda event: form_action_handler(self, event),
-            events.CallbackQuery(pattern=r"form_.*"),
+            self.form_action_handler,
+            CallbackQuery(pattern=r"form_.*"),
         )
 
     async def set_commands(self):
@@ -101,8 +232,10 @@ class MeowgramBot:
         If not, creates a new one.
         """
 
+        cat_client = self.cat_connections.get(user_id)
+
         # Create a new connection if one does not exist
-        if user_id not in self.cat_connections:
+        if not cat_client:
             cat_client = CheshireCatClient(
                 self.cat_url,
                 self.cat_port,
@@ -112,9 +245,6 @@ class MeowgramBot:
             # Store the connection in the dictionary
             self.cat_connections[user_id] = cat_client   
       
-        # Get the existing connection
-        cat_client = self.cat_connections[user_id] 
-        
         # Open a new connection if the WebSocket is closed
         if cat_client.ws is None or cat_client.ws.closed:
 
